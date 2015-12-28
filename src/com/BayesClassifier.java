cb0c87f228482cdb08863da3a380ddcbd7b1c28c
Brett <brett.lindsley@gmail.com>
2015-12-27 20:05:24 -0600

    Simple start of app.

---
+++ src/com/BayesClassifier.java
@@ -0,0 +1,15 @@
+package com;
+
+public class BayesClassifier {
+	
+	private void application() {
+		System.out.println("Test");
+		
+	}
+
+	public static void main(String[] args) {
+		BayesClassifier bc = new BayesClassifier();
+		bc.application();
+	}
+
+}
